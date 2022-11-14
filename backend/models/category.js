fa48f0e7f55d512e822d10ee4bcee08d2e5bdf6d
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-11-14 20:44:07 -0300

    Inicializacion frontend

--- backend/models/category.js
+++ backend/models/category.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        match: /^[a-zA-Z0-9]+$/,
     }
 })
 
