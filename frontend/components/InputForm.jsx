7c274a81a2ffdd2505ad8e8805896136f12b001e
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-12-05 21:45:55 -0300

    Se acondiciona componente para que pueda ser utilizado con formik

--- frontend/components/InputForm.jsx
+++ frontend/components/InputForm.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Input, FormControl, FormLabel } from '@chakra-ui/react'
 
-const InputForm = ({ label, handleChange, name, placeholder, type, value }) => {
+const InputForm = ({ label, handleChange, name, placeholder, type, value, handleBlur }) => {
+    console.log("name: ", name, "values: ", value)
     return (
         <FormControl id={name}>
             <FormLabel>{label}</FormLabel>
-            <Input type={type} placeholder={placeholder} name={name} onChange={handleChange} value={value} />
+            <Input type={type} name={name} onChange={handleChange} value={value} onBlur={handleBlur} placeholder={placeholder} />
         </FormControl>
     )
 }
